2814d58aa9353027ea761c769995186fef00c44a
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2022-01-16 15:03:36 +0100

    Teleporter added

---
+++ Lightmare/Assets/Scripts/Game/Teleporter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, target.position - transform.position, Color.magenta);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Teleport");
+            other.transform.position = target.position;
+        }
+    }
+}
