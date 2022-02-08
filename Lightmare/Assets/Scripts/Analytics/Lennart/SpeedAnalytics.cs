91a86ac3255e3950d65f1717db9f3c238d11ff33
boxheadt <43237433+boxheadt@users.noreply.github.com>
2022-02-08 15:41:53 +0100

    Speed analytics
    added speed analystics

--- Lightmare/Assets/Scripts/Analytics/Lennart/SpeedAnalytics.cs
+++ Lightmare/Assets/Scripts/Analytics/Lennart/SpeedAnalytics.cs
@@ -8,12 +8,20 @@ public class SpeedAnalytics : MonoBehaviour
 {
     Rigidbody playerRb;
     float highestSpeed;
+    float averageSpeed;
+    float interval;
+    float speedCollect;
+    float timeSpent;
+    float timesInter;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GameManager.Instance.Player.GetComponent<Rigidbody>();
         GameManager.Instance.failGame += OnFail;
         GameManager.Instance.finishGame += OnWin;
+        timeSpent = Time.deltaTime;
+
     }
     private void Update()
     {
@@ -21,15 +29,32 @@ public class SpeedAnalytics : MonoBehaviour
         {
             highestSpeed = playerRb.velocity.z;
         }
+        if (Time.deltaTime == interval)
+		{
+            speedCollect += playerRb.velocity.z;
+            timeSpent = 0;
+            timesInter++;
+		}
+        averageSpeed = speedCollect / timesInter;
     }
 
     void OnFail()
     {
         highestSpeed = 0f;
+        timeSpent = 0;
+        timesInter = 0;
+        speedCollect = 0f;
     }
 
     void OnWin()
     {
+        AnalyticsResult resultAvrg = Analytics.CustomEvent("AverageSpeedAnalytics",
+               new Dictionary<string, object> {
+                {"Level", SceneManager.GetActiveScene().name },
+                {"AverageSpeed", averageSpeed }
+               });
+     
+
         AnalyticsResult result = Analytics.CustomEvent("highestSpeedAnalytics",
             new Dictionary<string, object> {
                 {"Level", SceneManager.GetActiveScene().name },
